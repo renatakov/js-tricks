ca5f6a0b2b2179d5cdc5ea5fd71897eb9964e459
renatakov <renatakov1010@gmail.com>
2023-02-27 18:30:20 +0100

    slider added

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const inputFirstName = document.querySelector('.inputFirstName');
-const inputLastName = document.querySelector('.inputLastName');
-const btnSubmit = document.querySelector('.btnSubmit');
-const h1 = document.querySelector('h1');
+// const inputFirstName = document.querySelector('.inputFirstName');
+// const inputLastName = document.querySelector('.inputLastName');
+// const btnSubmit = document.querySelector('.btnSubmit');
+// const h1 = document.querySelector('h1');
 
-btnSubmit.addEventListener('click', () => {
-  localStorage.setItem('firstName', inputFirstName.value);
-  localStorage.setItem('lastName', inputLastName.value);
-})
-h1.innerText = `
-Hello ${localStorage.getItem('firstName')}
-${localStorage.getItem('lastName')}
-`;
+// btnSubmit.addEventListener('click', () => {
+//   localStorage.setItem('firstName', inputFirstName.value);
+//   localStorage.setItem('lastName', inputLastName.value);
+// })
+// h1.innerText = `
+// Hello ${localStorage.getItem('firstName')}
+// ${localStorage.getItem('lastName')}
+// `;
 
-console.table(localStorage);
+// console.table(localStorage);
---
+++ slider.js
@@ -0,0 +1,10 @@
+// const card = document.querySelectorAll('.card');
+// const prevBtn = document.querySelector('.prevBtn');
+// const nextBtn = document.querySelector('.nextBtn');
+
+// prevBtn.addEventListener('click', () => {
+
+// })
+// nextBtn.addEventListener('click', () => {
+
+//   })
