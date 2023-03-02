fca659480cf5b7af6f29c4cfce1a14576c548d87
renatakov <renatakov1010@gmail.com>
2023-03-02 11:37:51 +0100

    api

--- index.js
+++ index.js
@@ -13,3 +13,13 @@
 // `;
 
 // console.table(localStorage);
+
+// API
+const h1 = document.querySelector('h1');
+
+fetch('http://api.weatherapi.com/v1/current.json?key=f57ded1f279f4aeaa14102446232101&q=Krakov&aqi=no')
+.then(res => res.json())
+.then(data=>
+  h1.innerText = `${data.current.temp_c}°C in ${data.location.name}`
+  )
+  .catch(err => console.log(err));
