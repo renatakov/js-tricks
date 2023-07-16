4fadba12cbd4f2f9c6d3e45b0629fbf1ead7bda3
RENATA_PRO <renatakov1010@gmail.com>
2023-07-16 17:41:20 +0200

    all fixed

--- index.js
+++ index.js
@@ -14,8 +14,8 @@
 
 // console.table(localStorage);
 
-// API
-// const div = document.querySelector('div');
+// // API
+// const div = document.querySelector('div');.
 
 // fetch('https://newsdata.io/api/1/news?apikey=pub_181525573a2385a4c3925ecabbc34111be4a4')
 // .then(res => res.json())
@@ -33,34 +33,34 @@
 //   )
 //   .catch(err => console.log(err));
 
-// keyevents
+// // keyevents
 
-// const div = document.querySelector('div');
-// const btn  = document.querySelector('button');
-// let position = 0;
+// // const div = document.querySelector('div');
+// // const btn  = document.querySelector('button');
+// // let position = 0;
 
-// btn.addEventListener('keypress', (e) => {
-// if(e.key == 'd'){
-//   position+=50
-//   div.style.left = position + 'px';
-// }
-// if(e.key == 'a'){
-//   position+=-50
-//   div.style.left = position + 'px';
-// }
-// if(e.key == 'w'){
-//   position+=-50
-//   div.style.top = position + 'px';
-// }
-// if(e.key == 's'){
-//   position+=50
-//   div.style.top = position + 'px';
-// }
+// // btn.addEventListener('keypress', (e) => {
+// // if(e.key == 'd'){
+// //   position+=50
+// //   div.style.left = position + 'px';
+// // }
+// // if(e.key == 'a'){
+// //   position+=-50
+// //   div.style.left = position + 'px';
+// // }
+// // if(e.key == 'w'){
+// //   position+=-50
+// //   div.style.top = position + 'px';
+// // }
+// // if(e.key == 's'){
+// //   position+=50
+// //   div.style.top = position + 'px';
+// // }
 
 
-// })
+// // })
 
-// axios
+// // axios
 // const inputSearch = document.querySelector('#inputSearch');
 // let body = document.querySelector('body');
 // let h1 = document.querySelector('h1');
@@ -83,63 +83,63 @@
     
 // })
 
-const numbers = [11, 2, 55, 3]
+// const numbers = [11, 2, 55, 3]
 
-    numbers.sort((a, b) =>{
-    return b - a; // 55, 11, 3, 2
+//     numbers.sort((a, b) =>{
+//     return b - a; // 55, 11, 3, 2
     
-})
+// })
 
-console.log(numbers);
+// console.log(numbers);
 
-numbers.reduce((acc, currentValue) => {
-    return acc + currentValue;
-})
+// numbers.reduce((acc, currentValue) => {
+//     return acc + currentValue;
+// })
 
-console.log(numbers.reduce((acc, currentValue) => {
-    return acc + currentValue; // 71
-}))
+// console.log(numbers.reduce((acc, currentValue) => {
+//     return acc + currentValue; // 71
+// }))
 
-numbers.splice(0, 2, 33, 22)
-console.log(numbers); // 33, 22, 3, 2
+// numbers.splice(0, 2, 33, 22)
+// console.log(numbers); // 33, 22, 3, 2
 
-const numbers2 = numbers.slice(0, 2)
-console.log(numbers2); // [33, 22]
-const numbers3 = [1, 2, 13, 4];
+// const numbers2 = numbers.slice(0, 2)
+// console.log(numbers2); // [33, 22]
+// const numbers3 = [1, 2, 13, 4];
 
-console.log(numbers3.filter((a)=> a>3)) // 13, 4
+// console.log(numbers3.filter((a)=> a>3)) // 13, 4
 
-const numbers4 = [1, 2, 13, 4];
+// const numbers4 = [1, 2, 13, 4];
 
-console.log(numbers4.fill(10))// 10, 10, 10, 10
+// console.log(numbers4.fill(10))// 10, 10, 10, 10
 
-const users = [
-    {
-        id: 1,
-        name: 'John',
-        email: 'kenaa@example.com'
-    },
-    {
-        id: 2,
-        name: 'Jane',
-        email: 'kenaa@example.com'
-    },
-    {
-        id: 3,
-        name: 'Mike',
-        email: 'kenaa@example.com'
-    }
-]
+// const users = [
+//     {
+//         id: 1,
+//         name: 'John',
+//         email: 'kenaa@example.com'
+//     },
+//     {
+//         id: 2,
+//         name: 'Jane',
+//         email: 'kenaa@example.com'
+//     },
+//     {
+//         id: 3,
+//         name: 'Mike',
+//         email: 'kenaa@example.com'
+//     }
+// ]
 
 
 // console.log(users.filter((user) => { return user.name.toLocaleLowerCase().includes('j')}))
-console.log(users.filter((user) => { return user.id >= 2}))
-axios.get('https://jsonplaceholder.typicode.com/users')
-.then((res)=>{
-    console.log(res.data);
-    users = res.data;
+// console.log(users.filter((user) => { return user.id >= 2}))
+// axios.get('https://jsonplaceholder.typicode.com/users')
+// .then((res)=>{
+//     console.log(res.data);
+//     users = res.data;
     
-})
+// })
 
 // login
 const inputEmail = document.querySelector('.inputEmail')
--- slider.js
+++ slider.js
@@ -1,24 +1,24 @@
-// let btnPrev = document.getElementById("btnPrev");
-// let btnNext = document.getElementById("btnNext");
-// let sliderImg = document.getElementById("photo1");
+let btnPrev = document.getElementById("btnPrev");
+let btnNext = document.getElementById("btnNext");
+let sliderImg = document.getElementById("photo1");
 
-// let arrImg = ["0.jpg", "q93_cc6bcab1cdb51269a0272a6b1ed5b80c0ea170e263e1ff39571d6abfb5863b56.png", "witcher 3 007.jpg"];
+let arrImg = ["0.jpg", "q93_cc6bcab1cdb51269a0272a6b1ed5b80c0ea170e263e1ff39571d6abfb5863b56.png", "witcher 3 007.jpg"];
 
-// let imgNow = 0;
-// btnPrev.addEventListener("click",()=>{
-// if(imgNow==0){
-// imgNow = arrImg.length-1;
-// } else{
-//   imgNow--;
-// }
-// sliderImg.src = `/${arrImg[imgNow]}`;
+let imgNow = 0;
+btnPrev.addEventListener("click",()=>{
+if(imgNow==0){
+imgNow = arrImg.length-1;
+} else{
+  imgNow--;
+}
+sliderImg.src = `/${arrImg[imgNow]}`;
 
-// })
-// btnNext.addEventListener("click",()=>{
-//   if(imgNow==arrImg.length-1){
-//     imgNow = 0;
-//   } else{
-//     imgNow++;
-//   }
-// sliderImg.src = `/${arrImg[imgNow]}`;
-// });
+})
+btnNext.addEventListener("click",()=>{
+  if(imgNow==arrImg.length-1){
+    imgNow = 0;
+  } else{
+    imgNow++;
+  }
+sliderImg.src = `/${arrImg[imgNow]}`;
+});
