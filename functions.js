87914b17c329ecd488fb47e36dfbf25580004d6f
RENATA_PRO <renatakov1010@gmail.com>
2023-08-18 17:34:19 +0200

    1

---
+++ functions.js
@@ -0,0 +1,17 @@
+// 'use strict'
+// const func = () =>{
+// console.log(this)
+// // no use strict mode - window
+// //use strict mode - window
+// }
+
+// func()
+
+// function func2(){
+//     console.log(this)
+//     //no use strict mode - window
+//     //use strict mode - undefined
+// }
+
+// func2()
+
