23a45fb894d247294a26b92ceb36ff40b9e565d8
renatakov <renatakov1010@gmail.com>
2024-02-19 17:05:09 +0100

    1

--- index.js
+++ index.js
@@ -279,4 +279,47 @@
 // console.log(incrementString('fooobar099'));
 
 // ---------------------------------
+let nums = {
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+    'ten': 10,
+    'eleven': 11,
+    'twelve': 12,
+    'thirteen': 13,
+    'fourteen': 14,
+    'fifteen': 15,
+    'sixteen': 16,
+    'seventeen': 17,
+    'eighteen': 18,
+    'nineteen': 19,
+    'twenty': 20,
+    'thirty': 30,
+    'forty': 40,
+    'fifty': 50,
+    'sixty': 60,
+    'seventy': 70,
+    'eighty': 80,
+    'ninety': 90,
+    'hundred': 100,
+    'thousand': 1000,
+    'million': 1000000
 
+}
+
+function parseIntF(string) {
+    for (const num in nums) {
+        if(num === string){
+            return nums[num]
+        }
+    }
+}
+
+console.log(parseIntF('hundred'));
