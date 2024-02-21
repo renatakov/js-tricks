fcebd97148a40cfc911cf626df3ea6df734750dd
renatakov <renatakov1010@gmail.com>
2024-02-21 21:20:17 +0100

    1

--- index.js
+++ index.js
@@ -279,52 +279,72 @@
 // console.log(incrementString('fooobar099'));
 
 // ---------------------------------
-let nums = {
-    'zero': 0,
-    'one': 1,
-    'two': 2,
-    'three': 3,
-    'four': 4,
-    'five': 5,
-    'six': 6,
-    'seven': 7,
-    'eight': 8,
-    'nine': 9,
-    'ten': 10,
-    'eleven': 11,
-    'twelve': 12,
-    'thirteen': 13,
-    'fourteen': 14,
-    'fifteen': 15,
-    'sixteen': 16,
-    'seventeen': 17,
-    'eighteen': 18,
-    'nineteen': 19,
-    'twenty': 20,
-    'thirty': 30,
-    'forty': 40,
-    'fifty': 50,
-    'sixty': 60,
-    'seventy': 70,
-    'eighty': 80,
-    'ninety': 90,
-    'hundred': 100,
-    'thousand': 1000,
-    'million': 1000000
+// let nums = {
+//     'zero': 0,
+//     'one': 1,
+//     'two': 2,
+//     'three': 3,
+//     'four': 4,
+//     'five': 5,
+//     'six': 6,
+//     'seven': 7,
+//     'eight': 8,
+//     'nine': 9,
+//     'ten': 10,
+//     'eleven': 11,
+//     'twelve': 12,
+//     'thirteen': 13,
+//     'fourteen': 14,
+//     'fifteen': 15,
+//     'sixteen': 16,
+//     'seventeen': 17,
+//     'eighteen': 18,
+//     'nineteen': 19,
+//     'twenty': 20,
+//     'thirty': 30,
+//     'forty': 40,
+//     'fifty': 50,
+//     'sixty': 60,
+//     'seventy': 70,
+//     'eighty': 80,
+//     'ninety': 90,
+//     'hundred': 100,
+//     'thousand': 1000,
+//     'million': 1000000
 
-}
+// }
 
-function parseIntF(string) {
-    let arr = string.replace(/[-]/g, ' ').split(' ');
-    console.log(arr);
-    let newArr = arr.map((item)=>{
-        for (let num in nums) {
-            if (num === item) {
-                return nums[num]
-            }
-        }
-    })
-    return newArr
-}
+// function parseIntF(string) {
+//     let arr = string.replace(/[-]/g, ' ').split(' ');
+//     console.log(arr);
+//     let newArr = arr.map((item)=>{
+//         for (let num in nums) {
+//             if (num === item) {
+//                 return nums[num]
+//             }
+//         }
+//     })
+//     return newArr
+// }
 
-console.log(parseIntF('two hundred forty-six'));
+// console.log(parseIntF('two hundred forty-six'));
+
+// ----------------------------------
+
+// function phone(strng, num) {
+//     // your code
+// }
+
+const dr = "/+1-541-754-3010 156 Alphand_St. <J Steeve>\n 133, Green, Rd. <E Kustur> NY-56423 ;+1-541-914-3010\n"
++ "+1-541-984-3012 <P Reed> /PO Box 530; Pollocksville, NC-28573\n :+1-321-512-2222 <Paul Dive> Sequoia Alley PQ-67209\n"
++ "+1-741-984-3090 <Peter Reedgrave> _Chicago\n :+1-921-333-2222 <Anna Stevens> Haramburu_Street AA-67209\n"
++ "+1-111-544-8973 <Peter Pan> LA\n +1-921-512-2222 <Wilfrid Stevens> Wild Street AA-67209\n"
++ "<Peter Gone> LA ?+1-121-544-8974 \n <R Steell> Quora Street AB-47209 +1-481-512-2222\n"
++ "<Arthur Clarke> San Antonio $+1-121-504-8974 TT-45120\n <Ray Chandler> Teliman Pk. !+1-681-512-2222! AB-47209,\n"
++ "<Sophia Loren> +1-421-674-8974 Bern TP-46017\n <Peter O'Brien> High Street +1-908-512-2222; CC-47209\n"
++ "<Anastasia> +48-421-674-8974 Via Quirinal Roma\n <P Salinger> Main Street, +1-098-512-2222, Denver\n"
++ "<C Powel> *+19-421-674-8974 Chateau des Fosses Strasbourg F-68000\n <Bernard Deltheil> +1-498-512-2222; Mount Av.  Eldorado\n"
++ "+1-099-500-8000 <Peter Crush> Labrador Bd.\n +1-931-512-4855 <William Saurin> Bison Street CQ-23071\n"
++ "<P Salinge> Main Street, +1-098-512-2222, Denve\n"
+console.log(dr.match(/(?<=<)[A-Za-z]+\s[A-Za-z]+|(?<=<)[A-Za-z]+[A-Za-z]+/gm));
+console.log(dr.match(/\+\d{1,3}-\d{3}-\d{3}-\d{4}/gm));
