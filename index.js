8bf0730f1ba28e7509e6e9f9e0108310f41de24a
renatakov <renatakov1010@gmail.com>
2024-05-04 16:07:11 +0200

    1

--- index.js
+++ index.js
@@ -461,3 +461,24 @@ const dr = "/+1-541-754-3010 156 Alphand_St. <J Steeve>\n 133, Green, Rd. <E Kus
 // }
 
 // console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2))
+
+// -------------------------------
+
+// Three 1's => 1000 points
+// Three 6's =>  600 points
+// Three 5's =>  500 points
+// Three 4's =>  400 points
+// Three 3's =>  300 points
+// Three 2's =>  200 points
+// One   1   =>  100 points
+// One   5   =>   50 point
+
+function score(dice) {
+    let s = 0;
+    let obj = {}
+    dice.forEach(el => {
+        obj[el] = dice.filter(i => i === el).length;
+    })
+console.log(obj);
+}
+console.log(score([1, 1, 1, 3, 1]));
